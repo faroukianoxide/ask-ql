557a43713fa5e56f6ff60410ba3f2756037fcdd0
Farouk <ifarouk696@gmail.com>
2023-02-06 10:37:42 +0100

    change DB env variable

--- src/db.config.ts
+++ src/db.config.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 
 let pool: pg.Pool = new pg.Pool ({
-    connectionString: process.env.POSTGRES_CON_STRING
+    connectionString: process.env.DATABASE_URL
 })
 
 pool.on('connect', () => {
